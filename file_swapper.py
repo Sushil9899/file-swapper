1772122cb1815ef2d1d58c0940526277aaf32d12
Sushil9899 <89405154+Sushil9899@users.noreply.github.com>
2022-06-24 19:50:59 +0530

    Add files via upload

---
+++ file_swapper.py
@@ -0,0 +1,24 @@
+def swappingFile() :
+    file1 = input("write the file name here")
+    print("Selected.")
+
+    file2 = input("write the 2nd file name here")
+    print("Selected.")
+
+    with open(file1,'r') as a :
+        data_a = a.read()
+
+    with open(file2,'r') as b :
+        data_b = b.read()
+
+    with open(file1,'w') as a :
+            a.write(data_b)
+
+    with open(file2,'w') as b :
+            b.write(data_a)
+
+  
+    print("Done.")
+ 
+
+swappingFile()
